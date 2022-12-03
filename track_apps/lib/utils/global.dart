dd573c7434dfc80dde8a81361d55bd0ccacd7aac
hadlinar <hadlinar@gmail.com>
2022-12-03 15:02:07 +0700

    card pengiriman

--- track_apps/lib/utils/global.dart
+++ track_apps/lib/utils/global.dart
@@ -65,6 +65,75 @@ class Global {
     );
   }
 
+  static Card getCardPengiriman(String noPengiriman, int color, String status, String date, String jmlhFaktur) {
+    return Card(
+        elevation: 5,
+        shadowColor: const Color(0xffBCBCBC),
+        // color: Color(color),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15)
+        ),
+        child: SizedBox(
+            width: 370,
+            height: 130,
+            child: Container(
+              padding: const EdgeInsets.only(left: 12, top: 14),
+              child: Column(
+                children: [
+                  Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(top: 3),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(noPengiriman, style: Global.getCustomFont(Global.BLACK, 14, 'medium')),
+                          ),
+                        ),
+                        //TODO warna status
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Container(
+                            padding: const EdgeInsets.only(top: 3, bottom: 3, left: 9, right: 9),
+                            child: Text(status, style: Global.getCustomFont(Global.WHITE, 14, 'bold')),
+                            decoration: BoxDecoration(
+                              //TODO
+                                color: Color(color),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 14),
+                    child: const Divider(
+                      height: 2,
+                      thickness: 0.8,
+                      endIndent: 15,
+                      color: Color(0xff878787),
+                    )
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(date, style: Global.getCustomFont(Global.BLACK, 14, 'medium')),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(jmlhFaktur + " faktur", style: Global.getCustomFont(Global.BLACK, 14, 'medium')),
+                    ),
+                  )
+                ],
+              )
+            )
+        )
+    );
+  }
+
   static Text getMenuText(String textToShown) {
     return Text(
       textToShown,
--- track_apps/lib/view/list_pengiriman.dart
+++ track_apps/lib/view/list_pengiriman.dart
@@ -20,11 +20,31 @@ class _ListPengiriman extends State<ListPengiriman> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Color(0xffffffff),
-        child: Text('list pengiriman')
-      )
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Scaffold(
+          backgroundColor: const Color(0xffF7F7F7),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: false,
+            title: Text(
+                "Daftar Pengiriman",
+                style: Global.getCustomFont(Global.TOSCA, 18, 'bold')
+            ),
+          ),
+          body: Container(
+            padding: const EdgeInsets.only(top: 10),
+            width: double.infinity,
+            child: Column(
+              children: [
+                Container(
+                  child: Global.getCardPengiriman("TRK-2022-KP-0001", 0xff6FA9F7, "Barang belum dikirim", "16 Nov 2022, 13:57", "3"),
+                )
+              ],
+            )
+          )
+      ),
     );
   }
 }
